3734d649c95ef330e4f5ec2cf33d2d3c291a2c70
Jerome Heuze <jeromeheuze@gmail.com>
2022-02-15 09:44:43 -0800

    up

--- hprod-app/components/home-page-grid.vue
+++ hprod-app/components/home-page-grid.vue
@@ -3,7 +3,7 @@
 
     <div class="home-page-grid--cont">
       <h2>Our Games</h2>
-      <div class="game-grid">
+      <div class="game-grid middle-xs">
         <div class="card">
           <div class="card-img">
             <img src="/img/comingsoon.png" alt="" />
@@ -42,33 +42,6 @@
                 <div class="title-desc">
                   <h3>Hungry Mage - coming in 2022!</h3>
                   <p>Help our mage pick up his food in a certain order or more trouble will happen in the kingdom. But you are not alone!</p>
-                </div>
-              </div>
-              <div class="reviews">
-                <img src="/img/star-15.svg" alt="" />
-                <img src="/img/star-15.svg" alt="" />
-                <img src="/img/star-15.svg" alt="" />
-                <img src="/img/star-15.svg" alt="" />
-                <img src="/img/star-15.svg" alt="" /> (4.5)
-              </div>
-            </div>
-            <div class="info-download">
-              <p>Download now at</p>
-
-            </div>
-          </div>
-        </div>
-        <div class="card">
-          <div class="card-img">
-            <img src="/img/comingsoon.png" alt="" />
-          </div>
-          <div class="card-overlay">
-            <div class="info-cont">
-              <div class="icon-game-title">
-                <img src="/img/comingsoon_icon.png" alt="" />
-                <div class="title-desc">
-                  <h3>Solar Pioneer</h3>
-                  <p>Destroy and complete each levels in our version of space combat similar to Galaga arcade shumps. Coming in 2022!</p>
                 </div>
               </div>
               <div class="reviews">
