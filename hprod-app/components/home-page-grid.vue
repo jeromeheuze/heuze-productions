ff7c11c75b68385c5a910afbcc902d538799a864
Jerome Heuze <jeromeheuze@gmail.com>
2022-03-27 13:27:25 -0700

    added dates

--- hprod-app/components/home-page-grid.vue
+++ hprod-app/components/home-page-grid.vue
@@ -4,6 +4,7 @@
     <div class="home-page-grid--cont">
       <h2>Our Games</h2>
       <div class="game-grid middle-xs">
+
         <div class="card">
           <div class="card-img">
             <img src="/img/header-treasure-flip.jpg" alt="Treasure Flip: Halloween Edition" />
@@ -60,12 +61,14 @@
             </div>
           </div>
         </div>
+
       </div>
     </div>
 
     <div class="home-page-grid--cont">
       <h2>Our HTML5 Game Portals</h2>
       <div class="game-grid middle-xs">
+
         <div class="card">
           <div class="card-img">
             <a href="https://kohibou.com" target="_blank"><img src="/img/comingsoon.png" alt="Kohibou - 2D worlds" /></a>
@@ -76,15 +79,8 @@
                 <a href="https://kohibou.com" target="_blank"><img src="/img/comingsoon_icon.png" alt="Kohibou - 2D worlds" /></a>
                 <div class="title-desc">
                   <h3>Kohibou - 2D metaverse</h3>
-                  <p>Kohibou is a 2D pixel world with many environments to explore like a 2D metaverse. One character to explore many type of self-contain 2D worlds.</p>
+                  <p>Kohibou is a 2D pixel world with many environments to explore like a 2D metaverse. One character to explore many type of self-contain 2D worlds. To be Released in 2023.</p>
                 </div>
-              </div>
-              <div class="reviews">
-                <img src="/img/star-15.svg" alt="" />
-                <img src="/img/star-15.svg" alt="" />
-                <img src="/img/star-15.svg" alt="" />
-                <img src="/img/star-15.svg" alt="" />
-                <img src="/img/star-15.svg" alt="" /> (4.5)
               </div>
             </div>
           </div>
@@ -99,25 +95,20 @@
                 <a href="https://academialore.com" target="_blank"><img src="/img/comingsoon_icon.png" alt="Academia Lore - Game Portal" /></a>
                 <div class="title-desc">
                   <h3>Academia Lore - Game Portal</h3>
-                  <p>We are making a great product - a mix of education and video games - welcome to Edutainment and Mastery of skills.</p>
+                  <p>We are making a great product - a mix of education and video games - welcome to Edutainment and Mastery of skills. To be Released in 2023.</p>
                 </div>
-              </div>
-              <div class="reviews">
-                <img src="/img/star-15.svg" alt="" />
-                <img src="/img/star-15.svg" alt="" />
-                <img src="/img/star-15.svg" alt="" />
-                <img src="/img/star-15.svg" alt="" />
-                <img src="/img/star-15.svg" alt="" /> (4.5)
               </div>
             </div>
           </div>
         </div>
+
       </div>
     </div>
 
     <div class="home-page-grid--cont">
       <h2>Our Metaverse Development</h2>
       <div class="game-grid middle-xs">
+
         <div class="card">
           <div class="card-img">
             <a href="https://e2production.com" target="_blank"><img src="/img/e2production-social-banner.jpg" alt="E2Production" /></a>
@@ -128,15 +119,8 @@
                 <a href="https://e2production.com/" target="_blank"><img src="/img/e2production-96x96.png" alt="E2Production" /></a>
                 <div class="title-desc">
                   <h3>E2Production</h3>
-                  <p>Virtual businesses in the Earth2 Metaverse platform</p>
+                  <p>Virtual businesses in the Earth2 Metaverse platform.</p>
                 </div>
-              </div>
-              <div class="reviews">
-                <img src="/img/star-15.svg" alt="" />
-                <img src="/img/star-15.svg" alt="" />
-                <img src="/img/star-15.svg" alt="" />
-                <img src="/img/star-15.svg" alt="" />
-                <img src="/img/star-15.svg" alt="" /> (4.5)
               </div>
             </div>
           </div>
@@ -151,19 +135,13 @@
                 <a href="https://spaceship-adventure-park.com/" target="_blank"><img src="/img/spaceshipadventurepark-96x96.png" alt="Spaceship Adventure Park" /></a>
                 <div class="title-desc">
                   <h3>Spaceship Adventure Park</h3>
-                  <p>Welcome to the SpaceShip Adventure park. Building over 3 location inside Earth2.io - follow our development.</p>
+                  <p>Welcome to the SpaceShip Adventure park. Building over 3 location inside Earth2.io - follow our development. To be Released in 2023.</p>
                 </div>
-              </div>
-              <div class="reviews">
-                <img src="/img/star-15.svg" alt="" />
-                <img src="/img/star-15.svg" alt="" />
-                <img src="/img/star-15.svg" alt="" />
-                <img src="/img/star-15.svg" alt="" />
-                <img src="/img/star-15.svg" alt="" /> (4.5)
               </div>
             </div>
           </div>
         </div>
+
       </div>
     </div>
 
@@ -181,15 +159,8 @@
                 <a href="https://themes.forsale" target="_blank"><img src="/img/themesForSale-96x96.png" alt="Themes.ForSale" /></a>
                 <div class="title-desc">
                   <h3>Themes.ForSale</h3>
-                  <p>Purchase our quality themes for your favorite Jamstack framework like NuxtJS, Gatsby, Next, Hugo and Jekyll to accelerate your development time.</p>
+                  <p>Purchase our quality themes for your favorite Jamstack framework like NuxtJS, Gatsby, Next, Hugo and Jekyll to accelerate your development time. To be determined for release. Releasing 2022.</p>
                 </div>
-              </div>
-              <div class="reviews">
-                <img src="/img/star-15.svg" alt="" />
-                <img src="/img/star-15.svg" alt="" />
-                <img src="/img/star-15.svg" alt="" />
-                <img src="/img/star-15.svg" alt="" />
-                <img src="/img/star-15.svg" alt="" /> (4.5)
               </div>
             </div>
           </div>
@@ -204,15 +175,8 @@
                 <a href="https://jamstackhosting.reviews" target="_blank"><img src="/img/jamstackhosting-96x96.png" alt="JamstackHosting.Reviews" /></a>
                 <div class="title-desc">
                   <h3>JamstackHosting.Reviews</h3>
-                  <p>You have a Jamstack framework and need to launch - discover top Jamstack Hosting services from our list.</p>
+                  <p>You have a Jamstack framework and need to launch - discover top Jamstack Hosting services from our list. Releasing in 2022.</p>
                 </div>
-              </div>
-              <div class="reviews">
-                <img src="/img/star-15.svg" alt="" />
-                <img src="/img/star-15.svg" alt="" />
-                <img src="/img/star-15.svg" alt="" />
-                <img src="/img/star-15.svg" alt="" />
-                <img src="/img/star-15.svg" alt="" /> (4.5)
               </div>
             </div>
           </div>
@@ -238,13 +202,6 @@
                   <p>Entropia Universe general information - filled with useful tools to help every entropian players.</p>
                 </div>
               </div>
-              <div class="reviews">
-                <img src="/img/star-15.svg" alt="" />
-                <img src="/img/star-15.svg" alt="" />
-                <img src="/img/star-15.svg" alt="" />
-                <img src="/img/star-15.svg" alt="" />
-                <img src="/img/star-15.svg" alt="" /> (4.5)
-              </div>
             </div>
           </div>
         </div>
@@ -260,13 +217,6 @@
                   <h3>Entropia Market</h3>
                   <p>Market data from auction about deeds in Entropia Universe. And your first stop for virtual visits of Entropia estates.</p>
                 </div>
-              </div>
-              <div class="reviews">
-                <img src="/img/star-15.svg" alt="" />
-                <img src="/img/star-15.svg" alt="" />
-                <img src="/img/star-15.svg" alt="" />
-                <img src="/img/star-15.svg" alt="" />
-                <img src="/img/star-15.svg" alt="" /> (4.5)
               </div>
             </div>
           </div>
