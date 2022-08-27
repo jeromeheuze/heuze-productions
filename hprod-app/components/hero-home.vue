71e51a3289546a4c553b4be5128b8f5e819f0c3f
Spectrum3900 <jeromeheuze@gmail.com>
2022-08-27 11:21:08 -0700

    up

--- hprod-app/components/hero-home.vue
+++ hprod-app/components/hero-home.vue
@@ -2,6 +2,6 @@
   <div class="hero">
     <p class="red">Indie Game Studio & Web Development</p>
     <h1>Engaging Digital Products</h1>
-    <p>We are making 2D casual video games using Unity3D, Defold, GDevelop - mobile, TV, desktop, and metaverses. We are building a series of simple games with short stories and designed for any audience. We also developing fast websites and tools for online communities.</p>
+    <p>We are making 2D casual video games using Defold, GDevelop - mobile, TV, desktop, and metaverses. We are building a series of simple games with short stories and designed for any audience. We also developing fast websites and tools for online communities.</p>
   </div>
 </template>
